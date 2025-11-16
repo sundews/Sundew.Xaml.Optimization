b0cd035a3bf57379be5403dcf9f32b2fe37fdbe5
Kim Hugener-Ohlsen <kim@hugener.dk>
2025-11-16 23:47:21 +0100

    Support debugging optimizers

---
+++ Source/Sundew.Xaml.Optimization.Development.Tests/XamlFilesTests.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="XamlFilesTests.cs" company="Sundews">
+// Copyright (c) Sundews. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sundew.Xaml.Optimization.Development.Tests;
+
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using AwesomeAssertions;
+using NSubstitute;
+using Xunit;
+
+public class XamlFilesTests
+{
+    [Fact]
+    public async Task ForEachAsync_Then_AllFilesShouldBeProcessed()
+    {
+        string[] input = [GetXaml("Application"), GetXaml("UserControl"), GetXaml("Page"), GetXaml("Window")];
+        var testee =
+            new XamlFiles(
+                input.Select(x => new XamlFile(XDocument.Parse(x), Substitute.For<IFileReference>(), "\r\n")).ToArray(),
+                Environment.ProcessorCount);
+
+        var concurrentBag = new ConcurrentBag<XamlFile>();
+        await testee.ForEachAsync((xamlFile, cancellationToken) =>
+        {
+            concurrentBag.Add(xamlFile);
+            return Task.CompletedTask;
+        });
+
+        concurrentBag.Count.Should().Be(input.Length);
+    }
+
+    private static string GetXaml(string rootType)
+    {
+        return $@"<{rootType} x:Class=""Sundew.Xaml.Optimization""
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"" 
+    xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
+    xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
+    xmlns:sx=""http://sundew.dev/xaml""
+    mc:Ignorable=""d""
+    StartupUri=""MainWindow.xaml"">
+    <{rootType}.Resources>
+        <ResourceDictionary>
+            <ResourceDictionary.MergedDictionaries>
+                <ResourceDictionary Source=""/Sundew.Xaml.Optimization.Wpf;component/Controls.xaml"" d:sx.ResourceDictionary.Category=""🎨"" />
+            </ResourceDictionary.MergedDictionaries>
+        </ResourceDictionary>
+    </{rootType}.Resources>
+</{rootType}>";
+    }
+}
---
+++ Source/Sundew.Xaml.Optimization/OptimizerSettings.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OptimizerSettings.cs" company="Sundews">
+// Copyright (c) Sundews. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sundew.Xaml.Optimization;
+
+using System.Diagnostics;
+
+/// <summary>
+/// Represents configuration settings for controlling optimizer behavior, including debugging options.
+/// </summary>
+public class OptimizerSettings
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OptimizerSettings" /> class.
+    /// </summary>
+    /// <param name="debug">Indicates whether debugging features should be enabled for the optimizer. Set to <see langword="true"/> to
+    /// enable debugging; otherwise, <see langword="false"/>.</param>
+    public OptimizerSettings(bool debug)
+    {
+        this.Debug = debug;
+        if (debug)
+        {
+            Debugger.Launch();
+        }
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether the application is currently running in debugging mode.
+    /// </summary>
+    public bool Debug { get; }
+}
